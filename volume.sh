7b9e19e252e37aa9d126cfb51478b2cf9338bf7b
mafenet <32619250+mafenet@users.noreply.github.com>
2017-11-09 19:38:35 +0000

    Update volume.sh
    File to save used sink not needed anymore - pacmd

--- volume.sh
+++ volume.sh
@@ -2,25 +2,16 @@
 #
 # Control volume using the default or bluetooth device
 
-# variables: file with sink of bluetooth device and list of sinks
-sink_file=${HOME}/audio_sink
-
-IFS=$'\n'
-sinks=( $(pactl list sinks short) )
-
-# if nothing connected use default sink, otherwise sink of bluetooth device
-if [[ ${#sinks[@]} -eq 1 ]]; then
-	sink=0
-else
-	source ${sink_file}
-fi
+cmd=$1
+delta=5
+active_sink=$(pacmd list-sinks | awk '/* index:/{print $3}')
 
 # regulate Volume
-case "$1" in
-	"toggle")
-		pactl set-sink-mute $sink toggle;;
-	"high")
-		pactl set-sink-volume $sink +10%;;
-	"low")
-		pactl set-sink-volume $sink -10%;;
+case "$cmd" in
+	--toggle)
+		pactl set-sink-mute $active_sink toggle;;
+	--up)
+		pactl set-sink-volume $active_sink +$delta%;;
+	--down)
+		pactl set-sink-volume $active_sink -delta%;;
 esac
